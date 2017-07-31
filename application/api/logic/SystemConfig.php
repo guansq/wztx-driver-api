bf28ea50be7a8fc0ed5534860945c5897a3803d1
yangxueqin <865722714@qq.com>
2017-07-31 11:24:34 +0800

    config

--- application/api/logic/SystemConfig.php
+++ application/api/logic/SystemConfig.php
@@ -13,8 +13,10 @@
 // +----------------------------------------------------------------------
 
 namespace app\api\logic;
+
 use \think\Db;
 use app\common\model\SystemArea as SystemAreaModel;
+
 /**
  * 系统配置
  * Class Node
@@ -22,36 +24,45 @@ use app\common\model\SystemArea as SystemAreaModel;
  * @author Anyon <zoujingli@qq.com>
  * @date 2017/03/14 18:12
  */
-class SystemConfig extends BaseLogic{
+class SystemConfig extends BaseLogic {
 
-    public function getAppConfig(){
+    public function getAppConfig() {
         $config = [
-            'lastApkUrl' => getSysconf('last_apk_url',''),
-            'lastApkVersion' => getSysconf('last_apk_version',''),
-            'lastApkVersionNum' => intval(getSysconf('last_apk_version_num','')),
+            'lastApkUrl' => getSysconf('last_apk_url', ''),
+            'lastApkVersion' => getSysconf('last_apk_version', ''),
+            'lastApkVersionNum' => intval(getSysconf('last_apk_version_num', '')),
             'defaultAvatar' => 'http://opmnz562z.bkt.clouddn.com/b22e33502ca2e6e1d93a996e062e8f2d.png',
-            'payWays' => [
-                '货到验收合格，票到120天付款',
-                '货到验收合格，票到90天付款',
-                '货到验收合格，票到30天付款',
-                '预付30%，发货前付70%',
-                '预付30%，货到现场30%，安装调试合格30%，一年质保金10%',
-            ],
+            'share_percent' => getSysconf('share_percent'),
+            'grab_range' => getSysconf('grab_range'),
+            'premium_rate' => getSysconf('premium_rate'),
+            'bond_person_amount' => getSysconf('bond_person_amount'),
+            'bond_company_amount' => getSysconf('bond_company_amount'),
+            //'' => getSysconf(''),
+            'withdraw_begintime' => getSysconf('withdraw_begintime'),
+            'withdraw_endtime' => getSysconf('withdraw_endtime'),
+            'custom_phone' => getSysconf('custom_phone'),
+            'custom_email' => getSysconf('custom_email'),
+            'complain_phone' => getSysconf('complain_phone'),
+            'weixin_limit' => getSysconf('weixin_limit'),
+            'alipay_limit' => getSysconf('alipay_limit'),
+            'tran_account' => getSysconf('tran_account'),
+            'share_shipper' => getSysconf('share_shipper'),
+            'share_driver' => getSysconf('share_driver'),
             'xx' => '其他参数',
         ];
 
-        return resultArray(2000,'',$config);
+        return resultArray(2000, '', $config);
     }
 
-    public function getLastApk(){
+    public function getLastApk() {
 
         $lastApk = [
-            'url' => getSysconf('last_apk_url',''),
-            'version' => getSysconf('last_apk_version',''),
-            'versionNum' => getSysconf('last_apk_version_num',''),
+            'url' => getSysconf('last_apk_url', ''),
+            'version' => getSysconf('last_apk_version', ''),
+            'versionNum' => getSysconf('last_apk_version_num', ''),
         ];
 
-        return resultArray(2000,'',$lastApk);
+        return resultArray(2000, '', $lastApk);
     }
 
 }
